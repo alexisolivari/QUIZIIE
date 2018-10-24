cb8629ce00733dd2c9b60965f18e667d9d9554d5
titiyeahti <thibaut.milhaud@gmail.com>
2018-10-24 15:35:51 +0200

    Update app.component.ts

--- src/src/app/app.component.ts
+++ src/src/app/app.component.ts
@@ -8,7 +8,7 @@ import * as firebase from 'firebase';
 })
 export class AppComponent {
   constructor(){
-    /* var config = {
+     var config = {
       apiKey: "AIzaSyBeTrRPC8md2Ex_nM7uqR6n4URg4-jdkoM",
       authDomain: "projet-pima.firebaseapp.com",
       databaseURL: "https://projet-pima.firebaseio.com",
@@ -16,16 +16,6 @@ export class AppComponent {
       storageBucket: "projet-pima.appspot.com",
       messagingSenderId: "817238222604"
     };
-    firebase.initializeApp(config); */
-    // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyCrHZajpZ8sVdG12fTOYtJOj5E5i_HPJhw",
-      authDomain: "pima-test.firebaseapp.com",
-      databaseURL: "https://pima-test.firebaseio.com",
-      projectId: "pima-test",
-      storageBucket: "pima-test.appspot.com",
-      messagingSenderId: "609288607415"
-    };
-    firebase.initializeApp(config);
+    firebase.initializeApp(config); 
   }
 }
